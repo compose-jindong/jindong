21d01a338883f969d4303dad49346bbdc3a09327
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-01 20:09:49 +0900

    feat: apply binary compat plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.binaryCompatibilityValidator)
 }
 
 subprojects {
