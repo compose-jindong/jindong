9fa3e6b3b21013fa144c8d45453be605d10a45e4
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2026-01-01 20:31:04 +0900

    Project init (#1)
    * chore: set compose runtime, coroutines & init project
    
    * chore: remove useless files, add sample code
    
    * ci: remove unsupported platforms
    
    * feat: apply spotless
    
    * feat: apply binary compat plugin
    
    * ci: add spotless, api dump check
    
    * ci: add codeowners, set auto reviewer
    
    * fix: spotless apply

---
+++ jindong/build.gradle.kts
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2026 compose-jindong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "io.github.jindong"
+version = "1.0.0"
+
+kotlin {
+    androidLibrary {
+        namespace = "io.github.jindong"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        compilations.configureEach {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_17)
+                }
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.kotlinx.coroutines.core)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "jindong", version.toString())
+
+    pom {
+        name = "Jindong"
+        description = "Declarative Haptic Feedback Library using Compose Runtime for Kotlin Multiplatform"
+        inceptionYear = "2026"
+        url = "https://github.com/compose-jindong/jindong"
+        licenses {
+            license {
+                name = "Apache-2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0"
+            }
+        }
+        developers {
+            developer {
+                id = "l2hyunwoo"
+                name = "Hyunwoo Lee"
+            }
+            developer {
+                id = "wisemuji"
+                name = "Suhyeon Kim"
+            }
+        }
+        scm {
+            url = "https://github.com/compose-jindong/jindong"
+            connection = "scm:git:git://github.com/compose-jindong/jindong.git"
+            developerConnection = "scm:git:ssh://github.com/compose-jindong/jindong.git"
+        }
+    }
+}
