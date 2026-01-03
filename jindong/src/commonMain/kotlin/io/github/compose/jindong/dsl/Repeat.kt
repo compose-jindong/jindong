5c8a0f38f7ab904bd78ac334b9c15c1718d48014
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-03 22:17:03 +0900

    feat: impl Repeat composable

---
+++ jindong/src/commonMain/kotlin/io/github/compose/jindong/dsl/Repeat.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2026 compose-jindong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.compose.jindong.dsl
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeNode
+import io.github.compose.jindong.JindongScope
+import io.github.compose.jindong.compose.JindongApplier
+import io.github.compose.jindong.node.RepeatNode
+
+/**
+ * Repeats the content [count] times.
+ *
+ * Each repetition starts after the previous one completes.
+ *
+ * ```
+ * Jindong(trigger) {
+ *     Repeat(3) {
+ *         Haptic(50.ms)   // vibrates at 0ms, 50ms, 100ms
+ *     }
+ *
+ *     Repeat(2) {
+ *         Haptic(50.ms)
+ *         Delay(50.ms)    // 100ms per iteration
+ *     }
+ * }
+ * ```
+ *
+ * @param count Number of times to repeat
+ * @param content The pattern to repeat
+ */
+@Composable
+fun JindongScope.Repeat(
+  count: Int,
+  content: @Composable JindongScope.() -> Unit,
+) {
+  ComposeNode<RepeatNode, JindongApplier>(
+    factory = { RepeatNode(count) },
+    update = { },
+    content = { content() },
+  )
+}
