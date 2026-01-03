1613f8d88942521c27164c28fa8e2bbf5e622372
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-03 20:17:51 +0900

    chore: add compose compiler plugin dependency

--- jindong/build.gradle.kts
+++ jindong/build.gradle.kts
@@ -18,6 +18,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.vanniktech.mavenPublish)
     alias(libs.plugins.kotest)
     alias(libs.plugins.ksp)
