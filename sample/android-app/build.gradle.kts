c0524ecdd8d237257ee7b112ec60988d06dea0c2
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-06 22:54:42 +0900

    chore: set sample android-app, shared modules

---
+++ sample/android-app/build.gradle.kts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2026 compose-jindong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.compose.compiler)
+  alias(libs.plugins.composeMultiplatform)
+  kotlin("android")
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_17)
+  }
+}
+
+android {
+  namespace = "io.github.sample.android"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+  defaultConfig {
+    applicationId = "io.github.sample.android"
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.compileSdk.get().toInt()
+    versionCode = 1
+    versionName = "1.0.0"
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+}
+
+dependencies {
+  implementation(project(":sample:shared"))
+  implementation(libs.androidx.activity.compose)
+  implementation(compose.foundation)
+  implementation(compose.material3)
+}
