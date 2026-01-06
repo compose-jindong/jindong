c6fb0c2588a9d91965a2c843b7f7a7d8b1879644
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-06 19:56:02 +0900

    feat: impl IosHapticHandle

---
+++ jindong/src/iosMain/kotlin/io/github/compose/jindong/executor/HapticHandle.ios.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2026 compose-jindong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("ktlint:standard:filename")
+
+package io.github.compose.jindong.executor
+
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ObjCObjectVar
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import platform.CoreHaptics.CHHapticPatternPlayerProtocol
+import platform.Foundation.NSError
+
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+internal class IosHapticHandle(
+  private val player: CHHapticPatternPlayerProtocol?,
+  private val executor: DefaultIosHapticExecutor?,
+) : HapticHandle {
+
+  private var _isActive = player != null
+
+  override val isActive: Boolean
+    get() = _isActive
+
+  override fun cancel() {
+    if (!_isActive) return
+    _isActive = false
+
+    memScoped {
+      val errorPtr = alloc<ObjCObjectVar<NSError?>>()
+      player?.stopAtTime(0.0, errorPtr.ptr)
+    }
+    executor?.stopCurrentPlayer()
+  }
+}
