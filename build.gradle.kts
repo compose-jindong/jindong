2e423453f1c0c822a2b6a717b6c6273ed0a6a60b
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-07 00:40:33 +0900

    chore: add plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.binaryCompatibilityValidator) apply false
