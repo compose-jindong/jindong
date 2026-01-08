04f9e52a92c6ef344c4592e5ce81d7f5cdd0d132
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-09 01:28:52 +0900

    feat: add documentation

---
+++ documentation/lib/source.ts
@@ -0,0 +1,10 @@
+import { docs, meta } from "@/.source";
+import { resolveFiles } from "fumadocs-mdx";
+import { loader } from "fumadocs-core/source";
+
+export const source = loader({
+  baseUrl: "/docs",
+  source: {
+    files: resolveFiles({ docs, meta }),
+  },
+});
