eca4d6d22a1d8aa934a57f2dfaf0b7c8b57296fd
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2026-01-14 19:34:39 +0900

    [Documentation] Support i18n (#64)

--- documentation/lib/source.ts
+++ documentation/lib/source.ts
@@ -1,9 +1,11 @@
 import { docs, meta } from "@/.source";
 import { resolveFiles } from "fumadocs-mdx";
 import { loader } from "fumadocs-core/source";
+import { i18n } from "./i18n";
 
 export const source = loader({
   baseUrl: "/docs",
+  i18n,
   source: {
     files: resolveFiles({ docs, meta }),
   },
