9563964c048cbdc531f7738da6da74afc196df6d
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-01-22 23:48:03 +0900

    Feat(Core): Add DSL for haptic pattern construction

---
+++ jindong-core/src/commonMain/kotlin/io/github/compose/jindong/core/dsl/HapticDslMarker.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2026 compose-jindong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.compose.jindong.core.dsl
+
+/**
+ * DSL marker annotation for haptic pattern builder DSL.
+ *
+ * This annotation prevents implicit access to outer receivers when
+ * using nested DSL blocks, ensuring correct scoping of DSL functions.
+ */
+@DslMarker
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+public annotation class HapticDslMarker
